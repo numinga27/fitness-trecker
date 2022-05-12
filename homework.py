2e79ddbeb39ecb29e467770c707e3e36613bd2d0
Андрей Крылов <numinga27@yandex.ru>
2022-05-13 00:27:49 +0400

    текст комментария

--- homework.py
+++ homework.py
@@ -14,12 +14,7 @@ class InfoMessage:
         self.calories = calories
 
     def get_message(self) -> str:
-        return (f'Тип тренировки: {self.training_type}; '
-                f'Длительность: {self.duration:.3f} ч.;'
-                f'Дистанция: {self.distance:.3f} км; '
-                f'Ср. скорость: {self.speed:.3f} км/ч;'
-                f' Потрачено ккал: {self.calories:.3f}.')
-
+        return (f'Тип тренировки: {self.training_type}; Длительность: {self.duration:.3f} ч.; Дистанция: {self.distance:.3f} км; Ср. скорость: {self.speed:.3f} км/ч; Потрачено ккал: {self.calories:.3f}.') 
 
 class Training:
     """Базовый класс тренировки."""
@@ -129,8 +124,7 @@ def read_package(workout_type: str, data: list) -> Training:
 def main(training: Training) -> None:
     """Главная функция."""
     info = training.show_training_info()
-    it = info.get_message()
-    return print(it)
+    return print(info.get_message())
 
 
 if __name__ == '__main__':
